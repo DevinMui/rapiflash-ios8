b37718b2aaf796239e04f482d3e11b47357edb6e
Devin <devinmui@yahoo.com>
2015-05-26 13:55:12 -0700

    init

---
+++ Rapiflash.swift
@@ -0,0 +1,35 @@
+//
+//  Rapiflash.swift
+//  Rapiflash
+//
+//  Created by Devin Mui on 5/26/15.
+//  Copyright (c) 2015 Devin Mui. All rights reserved.
+//
+
+import Foundation
+typealias ServiceResponse = (JSON, NSError?) -> Void
+
+class RestApiManager: NSObject {
+    let sharedInstance = RestApiManager()
+    
+    let baseURL = "http://api.randomuser.me/"
+    
+    func getRandomUser(onCompletion: (JSON) -> Void) {
+        let route = baseURL
+        makeHTTPGetRequest(route, onCompletion: { json, err in
+            onCompletion(json as JSON)
+        })
+    }
+    
+    func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            let json:JSON = JSON(data: data)
+            onCompletion(json, error)
+        })
+        task.resume()
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rapiflash
+//
+//  Created by Devin Mui on 5/26/15.
+//  Copyright (c) 2015 Devin Mui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var idk: UITableView!
+
+}
+
